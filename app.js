d01075774c5906691582b0b741eb770a74da6ea0
Mads <mape23aq@student.cbs.dk>
2024-12-03 16:48:11 +0100

    "Hey"

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const app = express();
+//hey
 
 app.use(cors());
 app.use("/static", express.static("public"));
